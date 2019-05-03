03c70657abe98ee119691a7cb08e033105174a9c
Indra <indra.gunanda@gmail.com>
2019-05-03 19:00:53 +0700

    Migration Token

--- database/migrations/2019_05_02_090913_create_token.php
+++ database/migrations/2019_05_02_090913_create_token.php
@@ -15,9 +15,11 @@ class CreateToken extends Migration
     {
         Schema::create('token', function (Blueprint $table) {
           $table->string('id_token')->primary();
+          $table->string('issuer');
           $table->string('name');
           $table->double('decimal')->nullable();
           $table->string('icon')->nullable();
+          $table->text('desc')->nullable();
           $table->bigInteger("id_blockchain")->unsigned();
           $table->timestamps();
           $table->foreign('id_blockchain')->references('id_blockchain')->on('blockchain')->onDelete('cascade');
