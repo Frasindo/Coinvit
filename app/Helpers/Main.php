327aa26286caeab10975d7f32dbc4af96005a0ea
Indra <indra.gunanda@gmail.com>
2019-05-10 22:26:42 +0700

    Global FUnction

---
+++ app/Helpers/Main.php
@@ -0,0 +1,18 @@
+<?php
+function datatablesConvert($res=[],$select="")
+{
+  $data = [];
+  $data["data"] = [];
+  foreach ($res as $key => $value) {
+    $inner = [];
+    $exp = explode(",",$select);
+    foreach ($exp as $k => $v) {
+      $inner[] = $value["$v"];
+    }
+    $data["data"][] = $inner;
+  }
+  return $data;
+}
+function sort_vol($a, $b) {
+    return $a['volume'] < $b['volume'];
+}
