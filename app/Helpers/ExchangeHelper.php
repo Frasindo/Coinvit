4c317ca457d61102f29943107ba7125b35e718b8
Indra <indra.gunanda@gmail.com>
2019-05-11 14:50:43 +0700

    All Response CMC

--- app/Helpers/ExchangeHelper.php
+++ app/Helpers/ExchangeHelper.php
@@ -3,10 +3,13 @@ function spread($up,$down)
 {
   return (($up-$down)*100)/$up;
 }
-function convertCrypto($from="",$to="")
+function convertCrypto($from="",$to="",$all=false)
 {
   // $cmc = new CoinMarketCap\Api('yourApiClient');
   $cmc = new CoinMarketCap\Api('b0f49620-4e45-4080-86f9-658dfab7000e');
   $response = $cmc->cryptocurrency()->quotesLatest(['symbol' => $from, 'convert' => $to]);
+  if ($all) {
+    return $response->data->{$from}->quote->{$to};
+  }
   return $response->data->{$from}->quote->{$to}->price;
 }
