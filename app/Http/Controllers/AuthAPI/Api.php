1a447af5e4e8e6f9732ce20747705eae19acf088
Indra <indra.gunanda@gmail.com>
2019-05-21 11:43:03 +0700

    Trade Funnction Init

--- app/Http/Controllers/AuthAPI/Api.php
+++ app/Http/Controllers/AuthAPI/Api.php
@@ -3,11 +3,19 @@
 namespace Coinvit\Http\Controllers\AuthAPI;
 use Illuminate\Http\Request;
 use Coinvit\Http\Controllers\Controller;
-
+use Helpers\ArdorTrade;
 class Api extends Controller
 {
     public function index()
     {
       return response()->json(["status"=>1,"msg"=>"You Logged In"]);
+    }
+    public function trade(Request $req,$asset,$trade)
+    {
+      if ($trade == "ardor") {
+        
+      }else{
+        return response()->json(["status"=>0,"message"=>"Unknown Blockchain"],500);
+      }
     }
 }
