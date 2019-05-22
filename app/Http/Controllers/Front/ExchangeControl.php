7da13d44f897468ec07ced747dcaae900c63786b
Indra <indra.gunanda@gmail.com>
2019-05-22 07:06:53 +0700

    Detail Token

--- app/Http/Controllers/Front/ExchangeControl.php
+++ app/Http/Controllers/Front/ExchangeControl.php
@@ -20,13 +20,40 @@ class ExchangeControl extends Controller
         $infotoken = Token::where(["id_token"=>$asset]);
         $statisticToken = TokenStatistic::where(["id_token"=>$asset])->orderBy("created_at","desc");
         if ($infotoken->count() > 0) {
-          $infotoken = $infotoken->first();
-          if ($statisticToken->count() > 0) {
-            $statisticToken = $statisticToken->first();
+          $value = $infotoken->first();
+          $getStat = \Coinvit\TokenStatistic::where(["id_token"=>$asset])->orderBy("created_at","desc")->get();
+          $now = $getStat[0];
+          $vol = $now->volume;
+          $change_color = 'text-default';
+          $change_icon = '';
+          $change_value = 0;
+          if (!isset($getStat[1]->price)) {
+            $change_color = 'text-default';
+            $change_icon = '';
+            $change_value = 0;
           }else {
-            $statisticToken = ["volume"=>0,"price"=>0,"price_low"=>0,"price_high"=>0,"spread"=>0];
+            $change_value = ($now->price - $getStat[1]->price);
+            if ($change_value > 0) {
+              $change_icon = 'fa fa-caret-up';
+              $change_color = 'text-green';
+            }elseif($change_value < 0) {
+              $change_icon = 'fa fa-caret-down';
+              $change_color = 'text-red';
+            }
           }
-          return view("front.trade.tradebook",["title"=>$infotoken->name." - IGNIS","statistic"=>$statisticToken,"info"=>$infotoken]);
+          if ($value->icon != null) {
+            $icon = $value->icon;
+          }else {
+            $icon = url("assets/logo/blank.png");
+          }
+          $cekFav = \Coinvit\TokenFavorite::where(["id_token"=>$asset])->count();
+          $start = 'fa fa-star-o text-yellow';
+          if ($cekFav > 0) {
+            $start = "fa fa-star text-yellow";
+          }
+          $data = ["id_token"=>$asset,"icon_start"=>$start,"name"=>$value->name,"volume"=>$vol,"logo"=>$icon,"change_value"=>$change_value,"change_icon"=>$change_icon,"change_color"=>$change_color,"price"=>number_format($now->price,6),"l"=>number_format($now->price_low,6),"h"=>number_format($now->price_high,6)];
+
+          return view("front.trade.tradebook",["title"=>$data["name"]." - IGNIS","info"=>$data]);
         }else {
           return redirect("exchange");
         }
